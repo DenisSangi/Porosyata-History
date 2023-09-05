bf08922aa6e2c60618ddbc8111642aa47100f314
Denis Sangi <denissangi84@gmail.com>
2023-09-05 16:16:41 +0300

    Updating ContentView by refactoring Stack via ForEach instead of set of Button properties. Updating mainColor to be mutable depending of selecting answer via @State declaration.

--- Porosyata History/ContentView.swift
+++ Porosyata History/ContentView.swift
@@ -15,11 +15,12 @@ struct ContentView: View {
         correctAnswerIndex: 2
     )
     
-    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     
     var body: some View {
         ZStack {
             mainColor.ignoresSafeArea()
+            
             VStack {
                 Text("1 / 10")
                     .font(.callout)
@@ -30,33 +31,22 @@ struct ContentView: View {
                     .bold()
                     .multilineTextAlignment(.leading)
                 Spacer()
-                HStack{
+                
+                HStack {
+                    ForEach(0..<question.possibleAnswers.count, id: \.self) { answerIndex in
                     Button(action: {
-                        print("Tapped on Choice 1")
+                      print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
+                      mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
                     }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[0])
+                      ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
                     })
-                    Button(action: {
-                        print("Tapped on Choice 2")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[1])
-                    })
-                    Button(action: {
-                        print("Tapped on Choice 3")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[2])
-                    })
-                    Button(action: {
-                        print("Tapped on Choice 4")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[3])
-                    })
+                  }
                 }
-            }
+                }
+            }.foregroundColor(.white)
         }
-        .foregroundColor(.white)
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
